f5380830dce3ea9617ea304471c86ea771311c60
Stephen <stephen.lyssy@gmail.com>
2021-11-29 21:41:11 -0600

    Created Invoice and updated javascript to automatically enter the data fields in the invoice. Also updated the HTML to include the job name and number to be used in the invoice.

--- script.js
+++ script.js
@@ -15,7 +15,22 @@ const pmFeeResult = document.querySelector(".pm-fee-result");
 const resetButton = document.querySelector(".reset");
 const resultContainer = document.querySelector(".result-container");
 const pageContainer = document.querySelector(".page-container");
+const pageTitle = document.querySelector(".page-title");
+const pageSubtitle = document.querySelector(".page-subtitle");
 const calcBox = document.querySelector(".calc-box");
+const jobName = document.querySelector(".enter-job");
+const jobNumber = document.querySelector(".enter-job-number");
+const createInvoice = document.querySelector(".generate-invoice");
+const invoiceContainer = document.querySelector(".invoice-container");
+const jobNameEntry = document.querySelector("#enter-job");
+const jobNameTable = document.querySelector(".first-job");
+const jobNumTable = document.querySelector(".first-job-number");
+const jobNumEntry = document.querySelector("#enter-job-number");
+const salesCommissionTable = document.querySelector(".first-sales-commission");
+const pmFeeTable = document.querySelector(".first-pm-fee");
+const totalTableTop = document.querySelector(".first-total");
+const totalTableBottom = document.querySelector(".footer-total");
+const body = document.body;
 
 const showResults = function (event) {
   //   event.preventDefault();
@@ -34,6 +49,8 @@ calcButton.addEventListener("click", function (event) {
   const netMargin = netProfit / projectRevenue;
   const salesCommission = projectRevenue * grossMargin * 0.2;
   const pmFee = netProfit * netMargin;
+  const jobTableEntry = jobNameEntry.value;
+  const jobNumTableEntry = jobNumEntry.value;
 
   gpResult.textContent = `$${grossProfit.toFixed(2)}`;
   gmResult.textContent = `${(grossMargin * 100).toFixed(2)}%`;
@@ -42,12 +59,29 @@ calcButton.addEventListener("click", function (event) {
   netMarginResult.textContent = `${(netMargin * 100).toFixed(2)}`;
   salesCommResult.textContent = `$${salesCommission.toFixed(2)}`;
   pmFeeResult.textContent = `$${pmFee.toFixed(2)}`;
+  jobNameTable.textContent = `${jobTableEntry}`;
+  jobNumTable.textContent = `${jobNumTableEntry}`;
+  salesCommissionTable.textContent = `$${salesCommission.toFixed(2)}`;
+  pmFeeTable.textContent = `$${pmFee.toFixed(2)}`;
+  totalTableTop.textContent = `$${(+salesCommission + +pmFee).toFixed(2)}`;
+  totalTableBottom.textContent = `$${(+salesCommission + +pmFee).toFixed(2)}`;
 
   calcButton.classList.add("hidden");
   pageContainer.classList.add("background-off");
   calcBox.classList.add("hidden");
 
   showResults();
+});
+
+// # Adding event listener to the Create Invoice Button
+createInvoice.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  invoiceContainer.classList.remove("hide");
+  pageContainer.classList.add("hide");
+  pageTitle.classList.add("hide");
+  pageSubtitle.classList.add("hide");
+  body.classList.add("background-off");
 });
 
 // # Adding a the date to the date field on the invoice.
